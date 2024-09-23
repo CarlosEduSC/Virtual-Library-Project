fec803f297a4e9168a151c6ab690d30e76b064de
Carlos Eduardo <carloseduardodas.c@gmail.com>
2024-09-23 14:13:46 -0300

    adicionado função de buscar o livro pelo id

--- Back-End/src/main/java/com/example/demo/controllers/LivroController.java
+++ Back-End/src/main/java/com/example/demo/controllers/LivroController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,5 +63,14 @@ public class LivroController {
         }
 
         return ResponseEntity.ok().body(dadosLivros);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<DadosDetalharLivro> buscarLivroPeloId(@PathVariable String id) {
+        var livro = repository.findById(id).get();
+
+
+
+        return ResponseEntity.ok(new DadosDetalharLivro(livro));
     }
 }
